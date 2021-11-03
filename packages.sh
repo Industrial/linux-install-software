4e8b9d6fa604b70bb04d494ff4be2029e5f9fc0a
Tom Wieland <tom.wieland@gmail.com>
2021-11-03 15:13:02 +0100

    refactor(packages): disabled wayland for now

--- packages.sh
+++ packages.sh
@@ -17,12 +17,38 @@ conditionally_uninstall_packages $packages
 # TODO: when required
 echo "> packages > install required"
 packages=(
-  binutils
-  # list all packages in group, expanding it into this array
-  $(pacman -Qqg base-devel)
+  base-devel
   net-tools
   fakeroot
   git
   yay
 )
 conditionally_install_packages $packages
+
+echo "> packages > desktop"
+packages=(
+  clipman
+  otf-font-awesome
+  pipewire
+  vit
+  # alacritty
+  # bemenu-wayland
+  # sway
+  # waybar
+  # wl-clipboard # disable when using X
+  # xdg-desktop-portal-wlr
+  wmctrl
+)
+conditionally_install_packages $packages
+
+systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+systemctl --user enable --now pipewire.socket
+systemctl --user enable --now pipewire-media-session.service
+
+echo "> packages > nethogs"
+packages=(
+  nethogs
+)
+conditionally_install_packages $packages
+
+sudo setcap "cap_net_admin,cap_net_raw=ep" $(which nethogs)
