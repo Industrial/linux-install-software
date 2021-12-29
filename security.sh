ad97eefdaca96768f17a72d31d0d8ee593a3d993
Tom Wieland <tom.wieland@gmail.com>
2021-12-29 13:27:22 +0100

    fix(security): nordvpn setup

--- security.sh
+++ security.sh
@@ -25,13 +25,33 @@ packages=(
 )
 conditionally_install_packages $packages
 
-# Be sure to log in first
-sudo nordvpn set ipv6 true
+# Add user to nordvpn group
+sudo usermod -aG nordvpn $USER
+
+# Enable and start the service
+sudo systemctl enable nordvpnd
+sudo systemctl start nordvpnd
+
+# Setting this to true will leave you in a disconnected
+# state. While being safer, you won't be able to log in.
+sudo nordvpn set killswitch false
+
+# Log in
+ACCOUNT=$(sudo nordvpn account)
+ACCOUNT_NOT_LOGGED_IN="You are not logged in"
+# Match string (because of colors/control characters in output)
+if [[ "${ACCOUNT}" == *"${ACCOUNT_NOT_LOGGED_IN}"* ]]; then
+  sudo nordvpn login --legacy
+fi
+
+# Set nordvpn settings
+sudo nordvpn set autoconnect true
+sudo nordvpn set cybersec true
 sudo nordvpn set firewall true
+sudo nordvpn set ipv6 true
 sudo nordvpn set killswitch true
 sudo nordvpn set notify true
 sudo nordvpn set obfuscate true
-sudo nordvpn set cybersec true
-sudo nordvpn set autoconnect true
-sudo systemctl enable nordvpnd
-sudo systemctl start nordvpnd
+
+# Connect
+sudo nordvpn connect
