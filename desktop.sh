90fd0a0421437e60829954424716a44d51d7a511
Tom Wieland <tom.wieland@gmail.com>
2021-12-29 13:28:30 +0100

    fix(desktop): removed sway and wayland packages, because I'm using xfce

--- desktop.sh
+++ desktop.sh
@@ -8,16 +8,16 @@ packages=(
   # pipewire
   # alacritty
   # bemenu-wayland
-  sway
-  waybar
-  wl-clipboard # disable when using X
-  xdg-desktop-portal-wlr
+  # sway
+  # waybar
+  # wl-clipboard # disable when using X
+  # xdg-desktop-portal-wlr
   # clipman # interferes with tmux-yank!
   wmctrl
   chrome-gnome-shell
 )
 conditionally_install_packages $packages
 
-systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-systemctl --user enable --now pipewire.socket
-systemctl --user enable --now pipewire-media-session.service
+# systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+# systemctl --user enable --now pipewire.socket
+# systemctl --user enable --now pipewire-media-session.service
